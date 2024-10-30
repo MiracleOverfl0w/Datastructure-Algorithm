019fc8c3c889e9e8d83e025c2828de316b8e3ff0
MiracleOverfl0w <fiw.contact.work@gmail.com>
2024-10-30 15:50:25 +0700

    Update prime_posn.cpp

--- prime_posn.cpp
+++ prime_posn.cpp
@@ -3,7 +3,7 @@ int main(){
 	int mark = 2,n;
 	scanf("%d",&n);
 	int list[n-2];
-	for(int i = 2;i<=n;i++) list[i-2] = i;	//printf("%d %d\n",i,list[i-2]);
+	for(int i = 2;i<=n;i++) list[i-2] = i;
 
 	while(mark <= n){
 		for(int i = 0;i<n-2 ;i++){
@@ -12,6 +12,16 @@ int main(){
 		}
 		++mark;
 	}
-	int count = 0;
-
+	int i,j = 0 ,count[5];
+	for(i = 0;i < 5 ;i++) count[i] = 0;
+	for(i = sizeof(list)/sizeof(list[0])-1;i>=0 && j<5  ;i--) {
+		if(list[i] > 0 && j <5 ){
+			count[j] = list[i];
+			++j;
+		} 
+	}
+	for(i = sizeof(count)/sizeof(count[0])-1;i>=0;i--) {
+		if(count[i] > 0 ) printf("%d ",count[i]);
+	}
+	
 }
